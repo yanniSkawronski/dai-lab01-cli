9b3450a6769775a6f29de54cc2f2bd3356e67037
Tadeusz K. <tadeusz.kondracki@gmail.com>
2025-10-09 17:22:29 +0200

    add command name from jar name

--- src/main/java/ch/heigvd/Main.java
+++ src/main/java/ch/heigvd/Main.java
@@ -2,14 +2,22 @@ package ch.heigvd;
 
 import ch.heigvd.commands.Root;
 import picocli.CommandLine;
+import java.io.File;
 
 public class Main {
     public static void main(String[] args) {
+        // Define command name - source: https://stackoverflow.com/a/11159435
+        // (also java-ios dai project where this line is from)
+        String jarFilename =
+        new File(Main.class.getProtectionDomain().getCodeSource().getLocation().getPath())
+            .getName();
+
         Root root = new Root();
 
         int exitCode =
-                new CommandLine(root)
-                        .execute(args);
+            new CommandLine(root)
+                .setCommandName(jarFilename)
+                .execute(args);
 
         System.exit(exitCode);
 
